c1a9042dcc0b79890d2c874aa172c584a01b23c4
atsuki-sh <shinoto.atsuki31@gmail.com>
2021-07-29 16:56:45 +0900

    home画面を作り、ログインしたらhomeへいくように

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,8 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+//        $users = User::all();
+//
+//        return view('home', ['users' => $users]);
 
-        return view('home', ['users' => $users]);
+        return view('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,71 +2,40 @@
 
 @section('content')
 <div class="container">
-    <div class="row mb-3 justify-content-center">
-        <div class="col-md-8 d-flex justify-content-between align-items-center">
-            <h3>ユーザー管理画面</h3>
-        <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#exampleModal">新規登録</button>
-        </div>
-    </div>
-
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <ul class="list-group">
-                @foreach($users as $user)
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <h5 class="mb-0">{{ $user["name"] }}</h5>
-                        <span>
-                            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal">変更</button>
-                            <button type="button" class="btn btn-secondary">削除</button>
-                        </span>
-                    </li>
-                @endforeach
-            </ul>
+        <div class="col-md-5 mb-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">ユーザー管理</h5>
+                    <p class="card-text">ユーザーの管理画面です</p>
+                    <a href="#" class="btn btn-primary">移動</a>
+                </div>
+            </div>
         </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">ユーザー登録</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+        <div class="col-md-5 mb-4">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">お客様管理</h5>
+                    <p class="card-text">お客様の管理画面です</p>
+                    <a href="#" class="btn btn-primary">移動</a>
                 </div>
-                <div class="modal-body">
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">名前</span>
-                        </div>
-                        <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">メールアドレス</span>
-                        </div>
-                        <input type="text" class="form-control" aria-label="MailAddress" aria-describedby="basic-addon2">
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon3">パスワード</span>
-                        </div>
-                        <input type="text" class="form-control" aria-label="Password" aria-describedby="basic-addon3">
-                    </div>
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <label class="input-group-text" for="inputGroupSelect01">権限</label>
-                        </div>
-                        <select class="custom-select" id="inputGroupSelect01">
-                            <option value="0" selected>一般</option>
-                            <option value="1">管理者</option>
-                        </select>
-                    </div>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">配達管理</h5>
+                    <p class="card-text">配達の管理画面です</p>
+                    <a href="#" class="btn btn-primary">移動</a>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                    <button type="button" class="btn btn-primary">保存</button>
+            </div>
+        </div>
+        <div class="col-md-5">
+            <div class="card text-center">
+                <div class="card-body">
+                    <h5 class="card-title">ポイント管理</h5>
+                    <p class="card-text">ポイントの管理画面です</p>
+                    <a href="#" class="btn btn-primary">移動</a>
                 </div>
             </div>
         </div>
