11471f98c33b0943e0d66d830f7bdbd47ce4235d
atsuki-sh <shinoto.atsuki31@gmail.com>
2021-08-21 14:39:06 +0900

    顧客データをビューで表示。

--- resources/views/Customer/customer.blade.php
+++ resources/views/Customer/customer.blade.php
@@ -9,11 +9,40 @@
             <button type="button" class="btn btn-primary btn-lg" id="new" data-toggle="modal" data-target="#exampleModal">新規登録</button>
         </div>
 
-        <div class="d-flex ml-auto w-50 align-items-center">
+        <div class="d-flex mb-3 ml-auto w-50 align-items-center">
             <label for="input-search" class="sr-only">名前など</label>
+{{--            todo data-urlのrouteをお客様検索画面のものに変更--}}
             <input type="text" class="form-control mr-1 ml-auto w-50" id="input-search" data-url="{{ route('search_user') }}" placeholder="名前など">
             <button type="button" id="btn-search" class="btn btn-primary">検索</button>
         </div>
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">名前</th>
+                <th scope="col">電話番号</th>
+                <th scope="col">住所</th>
+                <th scope="col">部数</th>
+                <th scope="col">操作</th>
+            </tr>
+            </thead>
+            <tbody id="customer-list">
+            @foreach($customers as $customer)
+            <tr>
+                <th scope="row" class="align-middle">{{ $loop->iteration }}</th>
+                <td class="align-middle">{{ $customer->name }}</td>
+                <td class="align-middle">{{ $customer->tel }}</td>
+                <td class="align-middle">{{ $customer->address }}</td>
+                <td class="align-middle">{{ $customer->copy }}</td>
+                <td class="align-middle">
+                    <button type="button" class="btn btn-success change">変更</button>
+                    <button type="button" class="btn btn-secondary delete">削除</button>
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
 
